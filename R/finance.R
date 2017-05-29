8654dfadc2c8cd2cf6218f26918feb7dfa458db7
Kurt Hornik <Kurt.Hornik@R-project.org>
2017-05-29 15:40:56 +0000

    version 0.10-41

--- R/finance.R
+++ R/finance.R
@@ -132,200 +132,58 @@ function(x, pm = mean(x), riskless = FALSE, shorts = FALSE,
 }
 
 get.hist.quote <-
-function (instrument = "^gdax", start, end,
-          quote = c("Open", "High", "Low", "Close"),
-          provider = c("yahoo"), method = NULL,
-          origin = "1899-12-30", compression = "d",
-	  retclass = c("zoo", "ts"),
-	  quiet = FALSE, drop = FALSE)
-    ## Added new argument 'compression'.
-    ## May be "d", "w" or "m", for daily weekly or monthly.
-    ## Defaults to "d".
-    ## John Bollinger, 2004-10-27, www.BollingerBands.com, bbands@yahoo.com
-    ##
-    ## Changed POSIXct class to Date class, 2005-03-31
-    ## Added drop= arg, 2006-06-04
+function(instrument = "^gdax", start, end,
+         quote = c("Open", "High", "Low", "Close"),
+         provider = c("yahoo"), method = NULL,
+         origin = "1899-12-30", compression = "d",
+         retclass = c("zoo", "ts"),
+         quiet = FALSE, drop = FALSE)
 {
     if(missing(start)) start <- "1991-01-02"
     if(missing(end)) end <- format(Sys.Date() - 1, "%Y-%m-%d")
-  
+
     provider <- match.arg(provider)
     retclass <- match.arg(retclass)
+
+    periodicity <- match.arg(compression,
+                             c("daily", "weekly", "monthly"))
+
+    ## Be nice.
+    ind <- pmatch(quote, "AdjClose", nomatch = 0L)
+    quote[ind] <= "Adjusted"
 
     start <- as.Date(start)
     end <- as.Date(end)
 
-    if(is.null(method)) {
-        method <- getOption("download.file.method")
-        if(is.null(method))
-            method <- "auto"
+    x <- getSymbols(instrument, src = "yahoo",
+                    from = start, to = end, return.class = "zoo",
+                    periodicity = periodicity, auto.assign = FALSE)
+    
+    colnames(x) <- sub(".*\\.", "", colnames(x))
+    nser <- pmatch(quote, colnames(x))
+    if(any(is.na(nser)))
+        stop("this quote is not available")
+    n <- nrow(x)
+
+    dat <- index(x)
+    if(!quiet && (dat[1] != start))
+        cat(format(dat[1], "time series starts %Y-%m-%d\n"))
+    if(!quiet && (dat[n] != end))
+        cat(format(dat[n], "time series ends   %Y-%m-%d\n"))
+
+    if(retclass == "ts") {
+        jdat <- unclass(julian(dat, origin = as.Date(origin)))
+        ## We need unclass() because 1.7.0 does not allow adding a
+        ## number to a "difftime" object.
+        ind <- jdat - jdat[1] + 1
+        y <- matrix(NA, nrow = max(ind), ncol = length(nser))
+        y[ind, ] <- as.matrix(x[, nser, drop = FALSE])
+        colnames(y) <- colnames(x)[nser]
+        y <- y[, seq_along(nser), drop = drop]
+        return(ts(y, start = jdat[1], end = jdat[n]))
+    } else {
+        x[ , seq_along(nser), drop = drop]
     }
-
-    if(provider == "yahoo") {
-        url <-
-            paste("https://ichart.finance.yahoo.com/table.csv?s=",
-                  instrument, 
-                  format(start,
-                         paste("&a=",
-                               as.character(as.numeric(format(start, "%m"))-1),
-                               "&b=%d&c=%Y",
-                               sep = "")),
-                  format(end,
-                         paste("&d=",
-                               as.character(as.numeric(format(end, "%m"))-1),
-                               "&e=%d&f=%Y",
-                               sep = "")), 
-                  "&g=", compression,
-                  "&q=q&y=0&z=", instrument,
-                  "&x=.csv",
-                  sep = "")
-        destfile <- tempfile()
-
-        ## Try downloading several times.
-        ## Could make this controllable ...
-        i <- 1L
-        repeat {
-            status <- tryCatch(download.file(url, destfile,
-                                             method = method,
-                                             quiet = quiet),
-                               error = identity)
-            if(!inherits(status, "error") && (status == 0))
-                break
-            unlink(destfile)            
-            if(i >= 5L) {
-                if(inherits(status, "error"))
-                    stop(conditionMessage(status))
-                else
-                    stop(sprintf("download error, status %d", status))
-            }
-            message("download error, retrying ...")
-            i <- i + 1L
-        }
-        
-        nlines <- length(count.fields(destfile, sep = "\n"))
-        if(nlines == 1) {
-            unlink(destfile)
-            stop(paste("no data available for", instrument))
-        }
-        
-        ## Yahoo includes rows concerning dividends,
-        ## hence need fill = TRUE and na.omit
-        x <- read.table(destfile, header = TRUE, sep = ",", as.is = TRUE, fill = TRUE)
-        x <- na.omit(x)
-
-        ## As of 2012-07-07,
-        ##   get.hist.quote("ibm",  start = "2012-07-06", end = "2012-07-06")
-        ## gives two entries for 2012-07-06 ...
-        ## Fix (hopefully only needed temporarily?) suggested by AZ.
-        if(nrow(x) >= 2L && x[1L, 1L] == x[2L, 1L]) {
-            warning("first date duplicated, first instance omitted")
-            x <- x[-1L, , drop = FALSE]
-        }
-        
-        ## Debug
-        ## cat("read.table: start =", x[NROW(x),"Date"], "\n")
-        ## cat("read.table: end   =", x[1,"Date"], "\n")
-        
-        unlink(destfile)
-        
-        names(x) <- gsub("\\.", "", names(x))
-        nser <- pmatch(quote, names(x)[-1]) + 1
-        if(any(is.na(nser)))
-            stop("this quote is not available")
-        n <- nrow(x)
-
-        ## Yahoo now seems to format dates as %Y-%m-%d.
-        dat <- as.Date(as.character(x[, 1]), "%Y-%m-%d")
-        if(!quiet && (dat[n] != start))
-            cat(format(dat[n], "time series starts %Y-%m-%d\n"))
-        if(!quiet && (dat[1] != end))
-            cat(format(dat[1], "time series ends   %Y-%m-%d\n"))
-
-	if(retclass == "ts") {
-            jdat <- unclass(julian(dat, origin = as.Date(origin)))
-            ## We need unclass() because 1.7.0 does not allow adding a
-            ## number to a "difftime" object.
-            ind <- jdat - jdat[n] + 1
-            y <- matrix(NA, nrow = max(ind), ncol = length(nser))
-            y[ind, ] <- as.matrix(x[, nser, drop = FALSE])
-            colnames(y) <- names(x)[nser]
-	    y <- y[, seq_along(nser), drop = drop]
-            return(ts(y, start = jdat[n], end = jdat[1]))
-	} else {
-	  x <- as.matrix(x[, nser, drop = FALSE])
-	  rownames(x) <- NULL
-	  y <- zoo(x, dat)
-	  y <- y[, seq_along(nser), drop = drop]
-	  return(y)
-	}
-    }
-    ## <NOTE>
-    ## As of 2017-04, OANDA more or less requires registration.
-    ## (Apparently, one can still get <= 180 days of historical data via
-    ## JSON but not CSV, for the time being ...)
-    ## </NOTE>
-    ## else if(provider == "oanda") {
-    ##     if(!missing(quote)) {
-    ##         warning("argument 'quote' ignored for provider 'oanda'")
-    ##     }
-    ##     if(!missing(compression)) {
-    ##         warning("argument 'compression' ignored for provider 'oanda'")
-    ##     }
-    ##
-    ##     currencies <- unlist(strsplit(instrument, split = "/"))
-    ##     ranges <- c("d7", "d30", "d60", "d90", "d180", "y1", "y2", "y5")
-    ##     range <- ranges[c(7, 30, 60, 90, 180, 364, 728, 1820) >=
-    ##                         difftime(end, start, units="days")][1]
-    ##     url <- paste0("https://www.oanda.com/currency/historical-rates/download?",
-    ##                   "quote_currency=", currencies[1],
-    ##                   "&end_date=", end,
-    ##                   "&start_date=", start,
-    ##                   "&period=daily",
-    ##                   "&display=absolute",
-    ##                   "&rate=0",
-    ##                   "&data_range=", range,
-    ##                   "&price=mid",
-    ##                   "&view=table",
-    ##                   "&base_currency_0=", currencies[2],
-    ##                   "&base_currency_1=",
-    ##                   "&base_currency_2=",
-    ##                   "&base_currency_3=",
-    ##                   "&base_currency_4=",
-    ##                   "&download=csv")
-    ##     destfile <- tempfile()
-    ##
-    ##     status <- download.file(url, destfile, method = method, quiet = quiet)
-    ##     if(status != 0) {
-    ##         unlink(destfile)
-    ##         stop(paste("download error, status", status))
-    ##     }
-    ##
-    ##     x <- read.csv(destfile, skip = 4, as.is = TRUE, header = TRUE)
-    ##     unlink(destfile)
-    ##     x <- head(x, -3)
-    ##    
-    ##     dat <- rev(as.Date(x[[1]]))
-    ##     n <- length(dat)
-    ##     if(!quiet && (dat[1] != start))
-    ##         cat(format(dat[1], "time series starts %Y-%m-%d\n"))
-    ##     if(!quiet && (dat[n] != end))
-    ##         cat(format(dat[n], "time series ends   %Y-%m-%d\n"))
-    ##     val <- rev(x[[2]])
-    ##     if(is.character(val))
-    ##         val <- as.numeric(sub(",", "", val, fixed = TRUE))
-    ##
-    ##     if(retclass == "ts") {
-    ##         jdat <- unclass(julian(dat, origin = as.Date(origin)))
-    ##         ind <- jdat - jdat[1] + 1
-    ##         y <- rep.int(NA, max(ind))
-    ##         y[ind] <- val
-    ##         return(ts(y, start = jdat[1], end = jdat[n]))
-    ##     } else {
-    ##       y <- zoo(val, dat)
-    ##       return(y)
-    ##     }
-    ## }
-    else stop("provider not implemented")
 }
 
 maxdrawdown <-
